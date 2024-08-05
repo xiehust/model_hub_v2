db0bcd2cd5e4a81be9fbd3ed4125e0be537d785d
xie river <chuanxie@amazon.com>
2024-08-05 15:29:59 +0000

    add setup script

--- backend/01.setup.sh
+++ backend/01.setup.sh
@@ -15,9 +15,6 @@ pip install -r requirements.txt
 # 安装Docker
 sudo apt-get update
 sudo apt install docker python3-pip git -y && pip3 install -U awscli && pip install pyyaml==5.3.1
-
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt install docker-ce docker-ce-cli containerd.io -y
 # Configure components
 sudo systemctl enable docker && sudo systemctl start docker && sudo usermod -aG docker $USER
